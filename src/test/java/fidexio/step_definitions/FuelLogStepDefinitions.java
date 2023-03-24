4f5e0dbf6b7cd4d80eb34e2dbcabe756ca0c9f2a
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-03-24 22:16:55 +0300

    AC-6 Done Fuel Log

--- src/test/java/fidexio/step_definitions/FuelLogStepDefinitions.java
+++ src/test/java/fidexio/step_definitions/FuelLogStepDefinitions.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.WebElement;
 public class FuelLogStepDefinitions {
 
     HomePage homePage = new HomePage();
-    FuelLogPage fuelLogPage = new FuelLogPage();
+    FuelLogPage fp = new FuelLogPage();
 
     @When("user clicks More button")
     public void user_clicks_more_button() {
@@ -32,82 +32,99 @@ public class FuelLogStepDefinitions {
 
     @Then("user clicks Vehicles Fuel Logs button")
     public void user_clicks_vehicles_fuel_logs_button() {
-        fuelLogPage.vehiclesFuelLogsButton.click();
+        fp.vehiclesFuelLogsButton.click();
     }
 
     @Then("user clicks Create button")
     public void user_clicks_create_button() {
-        fuelLogPage.vehiclesFuelLogsCreateButton.click();
+        fp.vehiclesFuelLogsCreateButton.click();
     }
 
     @Then("user verifies Discard button visible")
     public void users_sees_discard_button() {
-        Assert.assertTrue(fuelLogPage.vehiclesFuelLogsDiscardButton.isDisplayed());
+        Assert.assertTrue(fp.vehiclesFuelLogsDiscardButton.isDisplayed());
     }
 
     @Then("users clicks Discard button")
     public void users_clicks_discard_button() {
-        fuelLogPage.vehiclesFuelLogsDiscardButton.click();
+        fp.vehiclesFuelLogsDiscardButton.click();
     }
 
     @Then("user verifies Create button visible")
     public void user_verifies_create_button_visible() {
-        Assert.assertTrue(fuelLogPage.vehiclesFuelLogsCreateButton.isDisplayed());
+        Assert.assertTrue(fp.vehiclesFuelLogsCreateButton.isDisplayed());
     }
 
     @Then("user clicks SAVE button")
     public void user_clicks_fuel_log_Save_Button() {
-        fuelLogPage.vehiclesFuelLogsSaveButton.click();
+        fp.vehiclesFuelLogsSaveButton.click();
     }
 
     @And("user verifies vehicle label invalid")
     public void user_verifies_vehicle_label_invalid() {
-        Assert.assertEquals(fuelLogPage.vehicleLabelInvalid, fuelLogPage.vehicleLabel.getAttribute("class"));
+        Assert.assertEquals(fp.vehicleLabelInvalid, fp.vehicleLabel.getAttribute("class"));
     }
 
     @Then("user enters {string} as vehicle name not in list")
     public void user_enters_new_vehicle_name_not_in_list(String string) {
-        fuelLogPage.vehicleInputBox.sendKeys(string);
+        fp.vehicleInputBox.sendKeys(string);
     }
 
     @Then("user select first vehicle from dropdown")
     public void user_enters_new_vehicle_name() {
-        fuelLogPage.vehicleInputBox.click();
-        fuelLogPage.vehicleNameSaved = fuelLogPage.vehicleSelectFirst.getText();
-        fuelLogPage.vehicleSelectFirst.click();
+        fp.vehicleInputBox.click();
+        fp.vehicleNameSaved = fp.vehicleSelectFirst.getText();
+        fp.vehicleSelectFirst.click();
     }
 
     @Then("user verifies create vehicle popup is displayed")
     public void user_verifies_create_vehicle_popup_is_displayed() {
-        Assert.assertTrue(fuelLogPage.createVehiclePopUp.isDisplayed());
+        Assert.assertTrue(fp.createVehiclePopUp.isDisplayed());
     }
 
     @Then("user verifies Edit button is seen")
     public void verify_edit_button_is_seen() {
-        BrowserUtils.waitForVisibility(fuelLogPage.vehiclesFuelLogsEditButton, 5);
-        Assert.assertTrue(fuelLogPage.vehiclesFuelLogsEditButton.isDisplayed());
+        BrowserUtils.waitForVisibility(fp.vehiclesFuelLogsEditButton, 5);
+        Assert.assertTrue(fp.vehiclesFuelLogsEditButton.isDisplayed());
     }
 
     @And("user delete last saved record")
     public void user_delete_last_saved_record() {
-        click(fuelLogPage.actionDropDown);
-        click(fuelLogPage.actionDropDownDeleteButton);
-        click(fuelLogPage.deleteOkButton);
+        click(fp.actionDropDown);
+        click(fp.actionDropDownDeleteButton);
+        click(fp.deleteOkButton);
     }
 
     @Then("user verifies saved details are seen")
     public void userVerifiesSavedDetailsAreSeen() {
-        Assert.assertEquals(fuelLogPage.vehicleNameSaved, fuelLogPage.detailInfoLabel.getText());
+        Assert.assertEquals(fp.vehicleNameSaved, fp.detailInfoLabel.getText());
     }
 
     @And("user verifies Create button is seen")
-    public void user_Verifies_Create_Button_Is_Seen() {
-        Assert.assertTrue(fuelLogPage.afterSaveCreateButton.isDisplayed());
+    public void user_verifies_create_button_is_seen() {
+        Assert.assertTrue(fp.afterSaveCreateButton.isDisplayed());
+    }
+
+    @And("user clicks Edit button")
+    public void user_clicks_Edit_button() {
+        fp.vehiclesFuelLogsEditButton.click();
+    }
+
+    @Then("user verifies details are seen and editable")
+    public void user_verifies_details_are_seen_and_editable() {
+        fp.literFieldBox.clear();
+        fp.literFieldBox.sendKeys("test123");
+        Assert.assertEquals("test123", fp.literFieldBox.getAttribute("value"));
+    }
+
+    @Then("user clicks ok button")
+    public void user_clicks_ok_button() {
+        fp.deleteOkButton.click();
     }
 
     public static void click(WebElement we) {
-        ((JavascriptExecutor) Driver.getDriver())
-                .executeScript("arguments[0].click()", we);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click()", we);
     }
+
 
 }
