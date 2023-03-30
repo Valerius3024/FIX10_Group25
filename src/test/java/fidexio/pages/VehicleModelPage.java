7d092b99cae286f54b729240c447e312fe738e4f
mesutdal <a.mesut.dal@gmail.com>
2023-03-30 04:03:53 +0300

    VehicleModel.feature VehicleModelPage, VehicleModel_StepDefinitions are finished

--- src/test/java/fidexio/pages/VehicleModelPage.java
+++ src/test/java/fidexio/pages/VehicleModelPage.java
@@ -1,29 +1,104 @@
 package fidexio.pages;
 
+import fidexio.utilities.BrowserUtils;
+import fidexio.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class VehicleModelPage {
 
+    public VehicleModelPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
-    @FindBy(xpath = "//*[contains(text(),'Vehicle Model')]")
+
+    @FindBy(css = "a[data-menu='136'] span[class='oe_menu_text']")
     public WebElement VehicleModelButton;
 
-    @FindBy(css = "input[placeholder*='Model']")
+    @FindBy(css = "button[class$='add']")
+    public WebElement createButton;
+
+    @FindBy(css = "input[class$='modifier']")
     public WebElement modelNameBox;
 
-    @FindBy(css = "input[placeholder*='T']")
+    @FindBy(css = "input[class$='ui-autocomplete-input']")
     public WebElement makeBox;
 
-    @FindBy(css = "span[class*='select']")
+    @FindBy(xpath = "//li[.='Create and Edit...']")
+    public WebElement createAndEditButton;
+
+    @FindBy(css = "html > body > div:nth-of-type(5) > div > div > div:nth-of-type(2) > div > div > table > tbody > tr > td > div > h1 > input")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//span[.='Save']")
+    public WebElement saveBrand;
+/*
+    @FindBy(xpath = "(//div/span)[8]")
     public WebElement imageUpload;
+
+ */
 
     @FindBy(css = "button[class$='save']")
     public WebElement saveButton;
 
+    @FindBy(css = "button[class$='edit']")
+    public WebElement editButton;
+
+    @FindBy(css = "button[accesskey='c']")
+    public WebElement newCreateButton;
+
+    @FindBy(xpath = "//th[.='Mustang (1)']")
+    public WebElement findVehicleModel;
+
+    @FindBy(xpath = "(//input[@type='checkbox'])[4]")
+    public WebElement checkboxVehicleModel;
+
+    @FindBy(xpath = "//*[contains(text(),'Action')]")
+    public WebElement action;
+
+    @FindBy(xpath = "//*[contains(text(),'Delete')]")
+    public WebElement deleteButton;
+
+    @FindBy(xpath = "//button[.='Ok']")
+    public WebElement confirmOkButton;
+
+    public void deleteVehicleModel(){
+        BrowserUtils.waitForVisibility(action,5);
+        action.click();
+
+        BrowserUtils.waitForVisibility(deleteButton,5);
+        deleteButton.click();
+
+        BrowserUtils.waitForVisibility(confirmOkButton,5);
+        confirmOkButton.click();
 
 
+    }
 
+    public void clearVehicleModel(){
+        BrowserUtils.waitFor(3);
+        VehicleModelButton.click();
+
+        if (findVehicleModel.isDisplayed()){
+            BrowserUtils.waitFor(3);
+            findVehicleModel.click();
+
+            BrowserUtils.waitForVisibility(checkboxVehicleModel,5);
+            checkboxVehicleModel.click();
+
+            BrowserUtils.waitForVisibility(action,5);
+            action.click();
+
+            BrowserUtils.waitForVisibility(deleteButton,5);
+            deleteButton.click();
+
+            BrowserUtils.waitForVisibility(confirmOkButton,5);
+            confirmOkButton.click();
+
+        }
+
+    }
 
 
 
--- src/test/resources/features/VehicleModel.feature
+++ src/test/resources/features/VehicleModel.feature
@@ -3,7 +3,7 @@ Feature: Creating a new vehicle model feature
   User Story: As a POSMANAGER, I should be able to create a new Vehicle Model.
 
   1. Verify that USER can create a new Vehicle Model.
-  2. User must choose at least a model name and a make to create a Vehicles Services Log.
+  2. User must choose at least a model name and a make to create a new Vehicle Model.
   3. After clicked the save button, the information of the Vehicle Model which user entered should be displayed.
   4. After clicked the Save button, Edit button display instead of Save button and Create button display instead of Discard button.
   5. When user clicks the Edit button the form should open by displaying the all the information and it be ready to edit.
@@ -14,11 +14,30 @@ Feature: Creating a new vehicle model feature
     Then User clicks login button
     And User navigates to fleet page
     And user clicks Vehicle Model button
-    And User clicks create button
+    And User clicks the create button
+    When user enters "Shelby 1967" model name
 
     Scenario: User should be able to create a new vehicle model
-      When user enters "Shelby 1967" model name
       And user enters "Mustang" brand name
-      And user adds the vehicle model image
-      And user clicks save button
+      And user clicks the save button
       Then User see the new vehicle model
+
+    Scenario: User should be able to create a new vehicle model at least one model name
+      And user clicks the save button
+      Then user should see the error message
+
+    Scenario: user should be able to see new vehicle model after the model creating
+      And user enters "Mustang" brand name
+      And user clicks the save button
+      Then User see the new vehicle model
+
+    Scenario: user should be able to see new vehicle model after the model creating
+      And user enters "Mustang" brand name
+      And user clicks the save button
+      Then user should see Edit and Create button
+
+    Scenario: user should see everything as editable after clicking the edit button
+      And user enters "Mustang" brand name
+      And user clicks the save button
+      Then user should see Edit and Create button
+      Then user should see everything as editable
